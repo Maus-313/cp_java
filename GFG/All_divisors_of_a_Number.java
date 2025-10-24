efcbf1db2eaabe70daf5c7f70c840ffbf4b8036d
Maus-landkreuzer <u0699522@gmail.com>
2025-10-24 22:14:01 +0530

    .

--- GFG/All_divisors_of_a_Number.java
+++ GFG/All_divisors_of_a_Number.java
@@ -4,32 +4,25 @@ import java.util.Collections;
 public class All_divisors_of_a_Number {
     public static void main(String[] args) {
         // 1, 2, 3
-        int n = 34;
+        int n = 2;
         print_divisors(n);
-        System.out.println((int)Math.sqrt(34));
+        // System.out.println((int)Math.sqrt(n));
     }
 
     public static void print_divisors(int n){
-        for(int i = 1; i<=n; i++){
-            if(n%i == 0){
-                System.out.print(i + " ");
-            }
-        }
+        
     }
 
     public static void sol1(int n) {
-        // code here
-        ArrayList<Integer> temp = new ArrayList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
         for(int i = 1; i*i<=n; i++){
             if(n%i == 0){
-                System.out.print(i+" ");
-                if(n/i != i) temp.add(n/i);
+                ans.add(i);
+                if(n/i != i) ans.add(n/i);
             }
         }
-
-        Collections.sort(temp);
-
-        for(int i : temp){
+        Collections.sort(ans);
+        for(int i : ans){
             System.out.print(i+" ");
         }
     }
