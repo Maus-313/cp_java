70dfc6d7083f403888685391575ae582f53181d7
Maus-landkreuzer <u0699522@gmail.com>
2025-10-25 13:26:52 +0530

    .

---
+++ GFG/Least_Prime_Factor.java
@@ -0,0 +1,10 @@
+public class Least_Prime_Factor {
+    public static void main(String[] args) {
+        
+    }
+
+    public int[] leastPrimeFactor(int n) {
+        // code here
+        
+    }
+}
--- LC/LC50.java
+++ LC/LC50.java
@@ -1,3 +1,43 @@
 public class LC50 {
-    
+    public static void main(String[] args) {
+        double x = -1.00000;
+        // int n = -2;
+        int n = Integer.MIN_VALUE;
+        // System.out.println(Integer.MIN_VALUE);
+        // System.out.println(-1*n);
+        System.out.println(new LC50().myPow(x,n));
+    }
+
+    public double myPow(double x, int n) {}
+
+    static double sol1(double x, int n){
+
+        if(n == 1) return x;
+        if(x == 1 || n == 0) return 1;
+        if(x == 0) return 0;
+
+        if(n < 0){
+            if(n == Integer.MIN_VALUE) return (Math.abs(x) != 1)? 0:1;
+            n = -1*n;
+            x = 1/x;
+            // 2^4 == 4^2 == 16^1
+            return sol1(x, n);
+        }else{
+            if(x<0){
+                x = -1*x;
+
+                if(n%2 == 0){
+                    return sol1(x, n);
+                }else{
+                    return -1*sol1(x, n);
+                }
+            }else{
+                if(n%2 == 0){
+                    return sol1(x*x, n/2);
+                }else{
+                    return x*sol1(x, n-1);
+                }
+            }
+        }
+    }
 }
