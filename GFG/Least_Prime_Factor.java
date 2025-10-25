a13d08905d3c1d47edc0db42a1792a5ce19a6f92
Maus-landkreuzer <u0699522@gmail.com>
2025-10-25 14:02:09 +0530

    .

--- GFG/Least_Prime_Factor.java
+++ GFG/Least_Prime_Factor.java
@@ -1,10 +1,42 @@
+import java.util.Arrays;
+
 public class Least_Prime_Factor {
     public static void main(String[] args) {
-        
+        int N = 12;
+        int[] ans = new Least_Prime_Factor().leastPrimeFactor(N);
+        for(int i :ans){
+            System.out.print(i+" ");
+        }
     }
 
-    public int[] leastPrimeFactor(int n) {
+    public int[] leastPrimeFactor(int n) {}
+
+
+
+    
+
+    public int[] sol1(int n) {
+        static int MAX = 100000;
+    static boolean init = false;
+    static int spf[] = new int[MAX+1];
         // code here
-        
+        if(!init) initialize();
+        return Arrays.copyOf(spf, n+1);
+
+    }
+
+    static void initialize(){
+        for(int i = 0; i<=MAX; i++){
+            spf[i] = i;
+        }
+
+        for(int i = 2; i*i<=MAX; i++){
+            if(spf[i] == i){
+                for(int j = i*i; j<=MAX; j+=i){
+                    if(spf[j] == j) spf[j] = i;
+                }
+            }
+        }
+        init = true;
     }
 }
