98f03d70856864050f8765b444cf2ac1e0ae12d1
Maus-landkreuzer <u0699522@gmail.com>
2025-12-10 23:06:28 +0530

    .

--- LC/LC540.java
+++ LC/LC540.java
@@ -4,13 +4,22 @@ public class LC540 {
     }
 
     public int singleNonDuplicate(int[] nums) {
-        int x = 0;
         
-        for(int i : nums){
-            x ^= i;
+    }
+
+    public int sol2(int[] nums) {
+        // 0ms
+        int l = nums.length;
+        if(l == 0) return -1;
+        if(l == 1) return nums[0];
+
+        if(nums[l-1] != nums[l-2]) return nums[l-1];
+
+        for(int i = 0; i<l-1; i+= 2){
+            if(nums[i] != nums[i+1]) return nums[i];
         }
 
-        return x;
+        return -1;
     }
 
     public int sol1(int[] nums) {
